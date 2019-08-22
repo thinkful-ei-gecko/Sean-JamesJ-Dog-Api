0626a08a3e80dd87ef1e2c1e1958ac868302a4d8
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-08-22 12:00:12 -0400

    add getImages function that defaults to 3 images in console

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
+'use strict';
+
 /**
  * All functions for grabbing dog image assets from DogAPI
  */
+
+// Fetch data from dogapi
+
+let getImages = function(num = 3) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+    .then((response) => response.json())
+    .then((jsonData) => console.log(jsonData));
+};
+
+getImages();
